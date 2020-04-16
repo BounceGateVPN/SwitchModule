dcf677d2d045c6b5adadf2351c08ecdc57120915
smallru8 <30855604+smallru8@users.noreply.github.com>
2020-04-16 21:17:37 +0800

    update

--- src/main/java/com/github/smallru8/BounceGateVPN/Switch/VirtualSwitch.java
+++ src/main/java/com/github/smallru8/BounceGateVPN/Switch/VirtualSwitch.java
@@ -125,14 +125,15 @@ public class VirtualSwitch extends Thread{
 			for(int k : port.keySet()) {
 				if(k!=tmpHashCode) {//不要送給自己
 					/*
-					 * 這裡會有個加密模組
+					 * 這裡會有個加密模組(如果port的type為WS)
 					 */
 					port.get(k).sendToDevice(data);
 				}
 			}
 		}else {//送給指定port
 			/*
-			 * 這裡會有個加密模組
+			 * 這裡會有個加密模組(如果port的type為WS)
+			 * 
 			 */
 			port.get(devHashCode).sendToDevice(data);
 		}
